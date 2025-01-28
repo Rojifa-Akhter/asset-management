5b278939661bc47ac52ccebd066ecfdfb9ea0e74
Rojifa <mstrojifaakhter@gmail.com>
2025-01-28 15:17:14 +0600

    fixed

--- database/migrations/2024_01_13_040749_create_assets_table.php
+++ database/migrations/2024_01_13_040749_create_assets_table.php
@@ -31,6 +31,7 @@ return new class extends Migration
             $table->string('location')->nullable();
             $table->float('residual_price')->nullable();
             $table->timestamps();
+            
         });
     }
 
